ca94cb1890c9466d75e7e3a8e1252164d233db41
keyide606 <251438278@qq.com>
2021-04-30 10:40:38 +0800

    使用Thread类各种api暴力结束线程的例子

--- src/com/lwl/thread/example/stop/StopThreadForce.java
+++ src/com/lwl/thread/example/stop/StopThreadForce.java
@@ -5,4 +5,21 @@ package com.lwl.thread.example.stop;
  * @date 2021-04-30 10:25
  */
 public class StopThreadForce {
+    public static void main(String[] args) {
+        ThreadService threadService = new ThreadService();
+
+        threadService.execute(() -> {
+            // 模拟加载一个很大的资源,超时
+//            while (true){
+//
+//            }
+            try {
+                Thread.sleep(5_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        threadService.shutdown(10_000);
+    }
 }
--- src/com/lwl/thread/example/stop/ThreadService.java
+++ src/com/lwl/thread/example/stop/ThreadService.java
@@ -5,4 +5,41 @@ package com.lwl.thread.example.stop;
  * @date 2021-04-30 10:24
  */
 public class ThreadService {
+
+
+    private Thread executeThread;
+
+    private boolean finished = false;
+
+    public void execute(Runnable runnable) {
+        executeThread = new Thread(() -> {
+            Thread t = new Thread(runnable);
+            t.setDaemon(true);
+            t.start();
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            finished = true;
+        });
+        executeThread.start();
+    }
+
+    public void shutdown(long mills)   {
+        long currentTime = System.currentTimeMillis();
+        while (!finished) {
+            if ((System.currentTimeMillis() - currentTime) > mills) {
+                System.out.println("任务超时,需要关闭它");
+                System.out.println(System.currentTimeMillis() - currentTime);
+                executeThread.interrupt();
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis() - currentTime);
+    }
 }
