9398f1ad5ac846c9689a56a8d6717cc2e0bf2e91
keyide606 <251438278@qq.com>
2021-04-30 11:38:21 +0800

    给银行叫号的例子增加同步锁保证线程安全

--- src/com/lwl/thread/example/bank/TicketWindow.java
+++ src/com/lwl/thread/example/bank/TicketWindow.java
@@ -1,18 +1,29 @@
 package com.lwl.thread.example.bank;
 
+
 /**
  * @author liwenlong
  * @date 2021-04-29 19:59
  */
 public class TicketWindow implements Runnable {
     private int index = 1;
-    private int MAX = 50;
-
+    private int MAX = 500;
 
     @Override
     public void run() {
-        while (index <= MAX) {
-            System.out.println(Thread.currentThread().getName() + "叫号:" + index++);
+        while (true) {
+            synchronized (this) {
+                if (index > MAX) {
+                    break;
+                }
+                System.out.println(Thread.currentThread().getName() + "叫号:" + index++);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
     }
 }
